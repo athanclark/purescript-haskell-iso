a0f8bc4060f87c5e1470713c75251dc7c9d0d344
Athan Clark <athan.clark@gmail.com>
2018-07-30 09:05:33 -0600

    date

---
+++ src/Data/Argonaut/JSONData.purs
@@ -0,0 +1,75 @@
+module Data.JSONDate where
+
+import Prelude
+import Data.Int as Int
+import Data.JSDate as JSDate
+import Data.Date (Date)
+import Data.Date as Date
+import Data.DateTime.Locale (LocalValue (..))
+import Data.Maybe (Maybe (..))
+import Data.Either (Either (..))
+import Data.Enum (fromEnum)
+import Data.String as String
+import Data.Generic (class Generic, gEq)
+import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson)
+import Data.Argonaut as Argonaut
+import Text.Parsing.StringParser (Parser, runParser)
+import Text.Parsing.StringParser as Parser
+import Text.Parsing.StringParser.String (regex)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Control.Monad.Eff.Now (NOW, nowDate)
+import Control.Monad.Eff.Exception (try)
+import Test.QuickCheck (class Arbitrary)
+
+
+newtype JSONDate = JSONDate Date
+
+derive instance genericJSONDate :: Generic JSONDate
+
+instance eqJSONDate :: Eq JSONDate where
+  eq = gEq
+
+getJSONDate :: JSONDate -> Date
+getJSONDate (JSONDate x) = x
+
+nowJSONDate :: forall eff. Eff (now :: NOW | eff) JSONDate
+nowJSONDate = do
+  LocalValue _ x <- nowDate
+  pure (JSONDate x)
+
+instance arbitraryJSONDate :: Arbitrary JSONDate where
+  arbitrary = pure $ unsafePerformEff nowJSONDate
+
+instance showJsonJSONDate :: Show JSONDate where
+  show (JSONDate x) =
+    let date' = JSDate.jsdate
+          { year: Int.toNumber $ fromEnum $ Date.year x
+          , month: Int.toNumber $ fromEnum (Date.month x) - 1
+          , day: Int.toNumber $ fromEnum $ Date.day x
+          , hour: 0.0
+          , minute: 0.0
+          , second: 0.0
+          , millisecond: 0.0
+          }
+    in  String.take 10 $ unsafePerformEff $ JSDate.toISOString date'
+
+instance encodeJsonJSONDate :: EncodeJson JSONDate where
+  encodeJson = encodeJson <<< show
+
+jsonDateParser :: Parser JSONDate
+jsonDateParser = do
+  s <- regex "\\d{4}-[01]\\d-[0-3]\\d"
+  case unsafePerformEff $ try $ JSDate.parse s of
+    Left _ -> Parser.fail "Not a date"
+    Right x -> case JSDate.toDate x of
+      Nothing -> Parser.fail "Not a date"
+      Just y -> pure (JSONDate y)
+  
+
+instance decodeJsonJSONDate :: DecodeJson JSONDate where
+  decodeJson json = do
+    s <- decodeJson json
+    case runParser jsonDateParser s of
+      Left _ -> Argonaut.fail "Not a date"
+      Right x -> pure x
---
+++ src/Data/Argonaut/JSONDateTime.purs
@@ -0,0 +1,77 @@
+module Data.Argonaut.JSONDateTime where
+
+import Prelude
+import Data.Int as Int
+import Data.JSDate as JSDate
+import Data.Time as Time
+import Data.Date as Date
+import Data.DateTime (DateTime)
+import Data.DateTime as DateTime
+import Data.DateTime.Locale (LocalValue (..))
+import Data.Maybe (Maybe (..))
+import Data.Either (Either (..))
+import Data.Enum (fromEnum)
+import Data.Generic (class Generic, gEq)
+import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson)
+import Data.Argonaut as Argonaut
+import Text.Parsing.StringParser (Parser, runParser)
+import Text.Parsing.StringParser as Parser
+import Text.Parsing.StringParser.String (regex)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Control.Monad.Eff.Now (NOW, nowDateTime)
+import Control.Monad.Eff.Exception (try)
+import Test.QuickCheck (class Arbitrary)
+
+
+newtype JSONDateTime = JSONDateTime DateTime
+
+derive instance genericJSONDateTime :: Generic JSONDateTime
+
+instance eqJSONDateTime :: Eq JSONDateTime where
+  eq = gEq
+
+getJSONDateTime :: JSONDateTime -> DateTime
+getJSONDateTime (JSONDateTime x) = x
+
+nowJSONDateTime :: forall eff. Eff (now :: NOW | eff) JSONDateTime
+nowJSONDateTime = do
+  LocalValue _ x <- nowDateTime
+  pure (JSONDateTime x)
+
+instance arbitraryJSONDate :: Arbitrary JSONDateTime where
+  arbitrary = pure $ unsafePerformEff nowJSONDateTime
+
+instance showJSONDateTime :: Show JSONDateTime where
+  show (JSONDateTime x) =
+    let date'' = DateTime.date x
+        time'' = DateTime.time x
+        date' = JSDate.jsdate
+          { year: Int.toNumber $ fromEnum $ Date.year date''
+          , month: Int.toNumber $ fromEnum (Date.month date'') - 1
+          , day: Int.toNumber $ fromEnum $ Date.day date''
+          , hour: Int.toNumber $ fromEnum $ Time.hour time''
+          , minute: Int.toNumber $ fromEnum $ Time.minute time''
+          , second: Int.toNumber $ fromEnum $ Time.second time''
+          , millisecond: Int.toNumber $ fromEnum $ Time.millisecond time''
+          }
+    in  unsafePerformEff $ JSDate.toISOString date'
+
+instance encodeJsonJSONDateTime :: EncodeJson JSONDateTime where
+  encodeJson = encodeJson <<< show
+
+jsonDateTimeParser :: Parser JSONDateTime
+jsonDateTimeParser = do
+  s <- regex "\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d+([+-][0-2]\\d:[0-5]\\d|Z)"
+  case unsafePerformEff $ try $ JSDate.parse s of
+    Left _ -> Parser.fail "Not a datetime"
+    Right x -> case JSDate.toDateTime x of
+      Nothing -> Parser.fail "Not a datetime"
+      Just y -> pure (JSONDateTime y)
+
+instance decodeJsonJSONDateTime :: DecodeJson JSONDateTime where
+  decodeJson json = do
+    s <- decodeJson json
+    case runParser jsonDateTimeParser s of
+      Left _ -> Argonaut.fail "not a datetime"
+      Right x -> pure x
